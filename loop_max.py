6d24047a79781c47eae82283615507b54e54cf84
kenzo2017 <mustardseed19@gmail.com>
2022-05-31 11:09:27 +0300

    using for loops to manipulate list

---
+++ loop_max.py
@@ -0,0 +1,11 @@
+# In this project i used for loop with if function to find the maximum score without using the max() and min()
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score of students is: {highest_score}")
